35fd39b47863de9dde160983af20e84bcf6111f3
Ekaterina Sadykova <strugovaeg@mail.ru>
2021-07-11 21:39:13 +0500

    perfectpixel

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
-// const contactButton = document.querySelector('.button-contacts');
-// const contactModal = document.querySelector('.modal-login');
-// const contactModalClose = contactModal.querySelector('.modal-close')
-
-// contactButton.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   contactModal.style.display = "block"
-// })
-
-// contactModalClose.addEventListener('click', (e) => {
-//   contactModal.style.display = "none"
-// })
-
-
-
 const catalogButtons = document.querySelectorAll('.buy');
 const catalogModal = document.querySelector('.modal-catalog');
 const catalogModalClose = catalogModal.querySelector('.modal-close')
-
-
 
 
 Array.from(catalogButtons).forEach(b => {
